5b353a6e896d7f40fa295bc1631e18c2e7d36874
Qunhao S <qunhaosong@gmail.com>
2017-05-18 21:07:38 -0500

    First commit

---
+++ client/components/layout/FooterBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="content has-text-centered">
+        <p>
+          AERO PAYMENTS, LLC. ALL RIGHTS RESERVED 2017
+        </p>
+        <p>
+          MADE IN CHICAGO, ILLINOIS USA
+        </p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+  export default {
+
+    data () {
+      return this.$store.state.pkg
+    }
+
+  }
+</script>
+
+<style lang="scss">
+  @import '~bulma/sass/utilities/mixins';
+
+  .footer {
+    margin-left: 180px;
+
+    @include mobile() {
+      margin-left: 0;
+    }
+    
+
+  }
+</style>
